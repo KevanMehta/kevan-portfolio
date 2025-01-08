a77b3ddfc769b4a87951b32671ff08f29f1ed912
Kevan Mehta <kevanmehta2511@gmail.com>
2025-01-07 18:08:12 -0600

    installed framer-motion & other dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
+import withBundleAnalyzer from '@next/bundle-analyzer';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+  reactStrictMode: true,
+});
 
 export default nextConfig;
